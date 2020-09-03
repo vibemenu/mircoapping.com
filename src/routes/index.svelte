ec8ecac6f5dacdde09f526287c734589b7a406bd
filipot <filipot@stud.ntnu.no>
2020-09-03 14:45:49 +0200

    m

---
+++ src/routes/index.svelte
@@ -0,0 +1,31 @@
+
+<svelte:head>
+	<title>microapping</title>
+</svelte:head>
+
+<main>
+    <p>microapping.com is a free static website hosting service primarily used for microapps on the vibe.menu website</p>
+    <p>Automatically host your svelte apps through the use of the vibemenu github app</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/routes/support.svelte
@@ -0,0 +1,5 @@
+<svelte:head>
+	<title>Support</title>
+</svelte:head>
+
+<p>For customer support, send an email to filip@vibe.menu</p>
